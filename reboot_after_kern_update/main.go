e9a506a386d108d380ec3332af0278f9be5b3876
Yaroslav Koisa <yaroslav@gateway-it.com>
2022-02-09 19:10:23 +0000

    Added JSON output

--- reboot_after_kern_update/main.go
+++ reboot_after_kern_update/main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os/exec"
 	"strings"
 
 	"github.com/facette/natsort"
 )
+
+type json_response_bad struct {
+	Status         int
+	Running_kernel string
+	Latest_kernel  string
+}
+
+type json_response_good struct {
+	Status         int
+	Running_kernel string
+}
 
 func main() {
 	//Set terminal output colors
@@ -18,14 +30,21 @@ func main() {
 	var running_kernel = cmdUname()
 	var latest_kernel = cmdLsBoot()
 
+	var status int
 	//Compare console outputs
 	if running_kernel == latest_kernel {
 		fmt.Println(colorGreen + "🟢 All good. You are running the latest kernel version." + colorReset)
+		status = 0
+		var jsonRespose = jsonResposeGood(status, running_kernel)
+		fmt.Println(jsonRespose)
 	} else {
 		fmt.Println(colorRed + "🔴 Please reboot to apply the kernel update!" + colorReset)
 		fmt.Println()
 		fmt.Println("You are running:      " + colorRed + running_kernel + colorReset)
 		fmt.Println("The latest installed: " + colorGreen + latest_kernel + colorReset)
+		status = 1
+		var jsonRespose = jsonResposeBad(status, running_kernel, latest_kernel)
+		fmt.Println(jsonRespose)
 	}
 }
 
@@ -69,4 +88,25 @@ func cmdLsBoot() string {
 
 	var final_output = output_list[len(output_list)-1][8:]
 	return final_output
+}
+
+func jsonResposeBad(status int, running_kernel, latest_kernel string) string {
+	var json_response = &json_response_bad{
+		Status:         status,
+		Running_kernel: running_kernel,
+		Latest_kernel:  latest_kernel,
+	}
+
+	var final_json_response, _ = json.Marshal(json_response)
+	return string(final_json_response)
+}
+
+func jsonResposeGood(status int, running_kernel string) string {
+	var json_response = &json_response_good{
+		Status:         status,
+		Running_kernel: running_kernel,
+	}
+
+	var final_json_response, _ = json.Marshal(json_response)
+	return string(final_json_response)
 }
