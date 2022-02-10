c3ffdea672cb6315813543a303c168878e33967f
Yaroslav Koisa <yaroslav@gateway-it.com>
2022-02-10 01:09:55 +0000

    Added command line flag "-json"

--- reboot_after_kern_update/main.go
+++ reboot_after_kern_update/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"os/exec"
 	"strings"
@@ -15,9 +16,30 @@ type json_response_bad struct {
 	Latest_kernel  string
 }
 
+func jsonResposeBad(status int, running_kernel, latest_kernel string) string {
+	var json_response = &json_response_bad{
+		Status:         status,
+		Running_kernel: running_kernel,
+		Latest_kernel:  latest_kernel,
+	}
+
+	var final_json_response, _ = json.Marshal(json_response)
+	return string(final_json_response)
+}
+
 type json_response_good struct {
 	Status         int
 	Running_kernel string
+}
+
+func jsonResposeGood(status int, running_kernel string) string {
+	var json_response = &json_response_good{
+		Status:         status,
+		Running_kernel: running_kernel,
+	}
+
+	var final_json_response, _ = json.Marshal(json_response)
+	return string(final_json_response)
 }
 
 func main() {
@@ -26,32 +48,46 @@ func main() {
 	const colorRed = "\033[31m"
 	const colorGreen = "\033[32m"
 
-	//Get console outputs
+	//Get command line flags
+	var jsonOutputFlag = flag.Bool("json", false, "Use JSON output")
+	flag.Parse()
+
+	//Run 2 functions to get console outputs for "uname -r" and "ls /boot"
 	var running_kernel = cmdUname()
 	var latest_kernel = cmdLsBoot()
 
+	//Init status variable for later
 	var status int
+
 	//Compare console outputs
 	if running_kernel == latest_kernel {
-		fmt.Println(colorGreen + "🟢 All good. You are running the latest kernel version." + colorReset)
 		status = 0
-		var jsonRespose = jsonResposeGood(status, running_kernel)
-		fmt.Println(jsonRespose)
+		if *jsonOutputFlag {
+			var jsonRespose = jsonResposeGood(status, running_kernel)
+			fmt.Println(jsonRespose)
+		} else {
+			fmt.Println(colorGreen + " 🟢 All good. You are running the latest kernel version." + colorReset)
+		}
 	} else {
-		fmt.Println(colorRed + "🔴 Please reboot to apply the kernel update!" + colorReset)
-		fmt.Println()
-		fmt.Println("You are running:      " + colorRed + running_kernel + colorReset)
-		fmt.Println("The latest installed: " + colorGreen + latest_kernel + colorReset)
 		status = 1
-		var jsonRespose = jsonResposeBad(status, running_kernel, latest_kernel)
-		fmt.Println(jsonRespose)
+		if *jsonOutputFlag {
+			var jsonRespose = jsonResposeBad(status, running_kernel, latest_kernel)
+			fmt.Println(jsonRespose)
+		} else {
+			fmt.Println(colorRed + " 🔴 Please reboot to apply the kernel update!" + colorReset)
+			fmt.Println()
+			fmt.Println("You are running:      " + colorRed + running_kernel + colorReset)
+			fmt.Println("The latest installed: " + colorGreen + latest_kernel + colorReset)
+		}
 	}
 }
 
 func cmdUname() string {
+	//Get command line output
 	var cmd = "uname -r"
 	var out, err = exec.Command("bash", "-c", cmd).Output()
 
+	//Handle errors
 	if err != nil {
 		fmt.Printf("%s", err)
 	}
@@ -66,13 +102,16 @@ func cmdUname() string {
 }
 
 func cmdLsBoot() string {
+	//Get command line output
 	var cmd = "ls -1 /boot/ | grep \"vmlinuz\\|vmlinux\" | grep -v \"vmlinuz.old\\|vmlinux.old\""
 	var out, err = exec.Command("bash", "-c", cmd).Output()
 
+	//Handle errors
 	if err != nil {
 		fmt.Printf("%s", err)
 	}
 
+	//Parse the output
 	var output = strings.Split(string(out), "\n")
 
 	var output_list = []string{}
@@ -88,25 +127,4 @@ func cmdLsBoot() string {
 
 	var final_output = output_list[len(output_list)-1][8:]
 	return final_output
-}
-
-func jsonResposeBad(status int, running_kernel, latest_kernel string) string {
-	var json_response = &json_response_bad{
-		Status:         status,
-		Running_kernel: running_kernel,
-		Latest_kernel:  latest_kernel,
-	}
-
-	var final_json_response, _ = json.Marshal(json_response)
-	return string(final_json_response)
-}
-
-func jsonResposeGood(status int, running_kernel string) string {
-	var json_response = &json_response_good{
-		Status:         status,
-		Running_kernel: running_kernel,
-	}
-
-	var final_json_response, _ = json.Marshal(json_response)
-	return string(final_json_response)
 }
