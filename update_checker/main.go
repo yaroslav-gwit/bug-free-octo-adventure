052625a7f12db0e86354e9e9e5c334c954df0190
Yaroslav Koisa <yaroslav@gateway-it.com>
2022-02-14 10:56:11 +0000

    Test

--- update_checker/main.go
+++ update_checker/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"os/exec"
+	"strings"
 )
 
 func main() {
+	//All updates list
 	refresh_cmd := "sudo apt-get update"
 	var _, _ = exec.Command("bash", "-c", refresh_cmd).Output()
 
@@ -14,8 +16,18 @@ func main() {
 	all_updates_output := string(all_updates_out)
 	fmt.Println(all_updates_output)
 
+	//Sec updates list
 	security_updates_cmd := "sudo apt-get dist-upgrade -s | grep Inst | grep security"
 	var security_updates_out, _ = exec.Command("bash", "-c", security_updates_cmd).Output()
-	security_updates_output := string(security_updates_out)
-	fmt.Println(security_updates_output)
+
+	security_updates_output := strings.Split(string(security_updates_out), "\n")
+
+	var security_updates_list []string
+
+	for _, item := range security_updates_output {
+		if item != "" {
+			security_updates_list = append(security_updates_list, item)
+		}
+	}
+	fmt.Println(security_updates_list)
 }
