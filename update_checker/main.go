22814f6c3eb4977a308588dfa96d32b3160e692d
Yaroslav Koisa <yaroslav@gateway-it.com>
2022-02-14 12:36:37 +0000

    Test

--- update_checker/main.go
+++ update_checker/main.go
@@ -102,7 +102,7 @@ func Centos() UpdatesStruct {
 }
 
 func OsChecker() string {
-	cmd := "cat /etc/os-release | grep \"ID=\""
+	cmd := "cat /etc/os-release | grep \"ID=\" | grep -v \"VERSION_ID=\""
 	var os_release, _ = exec.Command("bash", "-c", cmd).Output()
 	final_output := string(os_release)
 	final_output = strings.ReplaceAll(final_output, "\n", "")
