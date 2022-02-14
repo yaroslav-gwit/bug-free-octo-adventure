237b6bcf3d2aeaeaf23aef888b071c3c7afd52c8
Yaroslav Koisa <yaroslav@gateway-it.com>
2022-02-14 11:02:57 +0000

    Test

--- update_checker/main.go
+++ update_checker/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os/exec"
+	"strconv"
 	"strings"
 )
 
@@ -22,7 +23,7 @@ func main() {
 			all_updates_list = append(all_updates_list, item)
 		}
 	}
-	fmt.Println(len(all_updates_list))
+	fmt.Println("There are: " + strconv.Itoa(len(all_updates_list)) + " updates available.")
 
 	//Sec updates list
 	security_updates_cmd := "sudo apt-get dist-upgrade -s | grep Inst | grep security"
@@ -37,5 +38,5 @@ func main() {
 			security_updates_list = append(security_updates_list, item)
 		}
 	}
-	fmt.Println(len(security_updates_list))
+	fmt.Println("Including " + strconv.Itoa(len(security_updates_list)) + " security updates!")
 }
